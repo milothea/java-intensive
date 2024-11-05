e87914089177fac4f8f2ea651561d3c7024824d3
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2024-11-05 22:26:11 +0100

    refactor: refactor creating orders/reservations logic, fix isEqual method in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import src.model.Order;
@@ -10,74 +11,84 @@ import src.model.ApartmentReservation;
 import src.constants.Actions;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("""
-            Enter 'apart' to book an apartment. Enter 'book' to open books' order.
+            Enter 'apart' to book an apartment. Enter 'book' to open books' order. Maximum 5 entries.book
             Enter 'stop' to quit the app.""");
 
         RegisterService registerService = new RegisterService();
         OrderService orderService = new OrderService();
         Scanner input = new Scanner(System.in);
         String action = input.nextLine();
+        ArrayList<Order> orders = new ArrayList<>();
+        ArrayList<ApartmentReservation> reservations = new ArrayList<>();
 
         do {
-            boolean stopFlag = false;
-
             if (isEqual(action, Actions.APART)) {
-                stopFlag = makeApartmentReservation(input, registerService);
+                ApartmentReservation reservation = makeApartmentReservation(input, registerService);
+
+                if (reservation == null) {
+                    action = Actions.STOP.toString();
+                } else {
+                    reservations.add(reservation);
+                }
             }
 
             if (isEqual(action, Actions.BOOK)) {
-                stopFlag = placeBooksOrder(input, orderService);
+                Order order = placeBooksOrder(input, orderService);
+
+                if (order == null) {
+                    action = Actions.STOP.toString();
+                } else {
+                    orders.add(order);
+                }
             }
 
-            if (stopFlag) {
-                break;
-            }
-        } while (!isEqual(action, Actions.STOP));
+        } while(!isEqual(action, Actions.STOP));
     }
 
-    private static boolean makeApartmentReservation(Scanner scanner, RegisterService service) {
+    private static ApartmentReservation makeApartmentReservation(Scanner scanner, RegisterService service) {
         System.out.println("Enter price for apartment reservation:");
         String userInput = scanner.nextLine();
 
-        if (isEqual(userInput, Actions.STOP)) {
-            return true;
+        if (!isEqual(userInput, Actions.STOP)) {
+            try {
+                BigDecimal price = new BigDecimal(userInput);
+                ApartmentReservation reservation = service.registerApartment(price);
+
+                System.out.println(reservation.getReservationData());
+
+                return reservation;
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong input. Please check. It should be number.");
+            }
         }
 
-        try {
-            BigDecimal price = new BigDecimal(userInput);
-            ApartmentReservation reservation = service.registerApartment(price);
-
-            System.out.println(reservation.getReservationData());
-        } catch (NumberFormatException e) {
-            System.out.println("Wrong input. Please check. It should be number.");
-        }
-
-        return false;
+        return null;
     }
 
-    private static boolean placeBooksOrder(Scanner scanner, OrderService service) {
+    private static Order placeBooksOrder(Scanner scanner, OrderService service) {
         System.out.println("Enter books for order using comma without spaces:");
         String userInput = scanner.nextLine();
 
-        if (isEqual(userInput, Actions.STOP)) {
-            return true;
+        if (!isEqual(userInput, Actions.STOP)) {
+            try {
+                String[] books = userInput.split(",");
+                Order order = service.createNewOrder(books);
+
+                System.out.println(order.getOrderData());
+
+                return order;
+            } catch (NullPointerException exception) {
+                System.out.println("Wrong input.");
+            }
         }
 
-        try {
-            String[] books = userInput.split(",");
-            Order newOrder = service.createNewOrder(books);
-
-            System.out.println(newOrder.getOrderData());
-        } catch (NullPointerException exception) {
-            System.out.println("Wrong input.");
-        }
-
-        return false;
+        return null;
     }
 
     private static boolean isEqual(String string, Actions action) {
-        return action.toString().toLowerCase().equals(string);
+        return action.toString().toLowerCase().equals(string.toLowerCase());
     }
 }
